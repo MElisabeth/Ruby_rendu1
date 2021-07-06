cef21c24c827af0366a92155dd112c5500928acb
MElisabeth <86658974+MElisabeth@users.noreply.github.com>
2021-07-06 17:32:50 +0200

    exo pyramide

---
+++ exo_15.rb
@@ -0,0 +1,13 @@
+puts "Salut, bienvenue dans ma super pyramide ! Combien d'étages veux-tu ?"
+print "> " 
+n = gets.chomp.to_i
+
+puts "Voici ma pyramide :"
+if n < 0
+  print 'Votre nombre n’est pas positif.'
+else
+  1.upto(n) do |k|
+    k.times { print "#" }
+    puts
+  end
+end  
